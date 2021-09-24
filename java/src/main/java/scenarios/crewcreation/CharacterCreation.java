14b0ca473639e9e5fa67a0c3715a90b572c20812
Jonathan Theriault <jt5880@aol.com>
2021-09-24 01:18:01 -0400

    Minor stylistic changes

--- java/src/main/java/scenarios/crewcreation/CharacterCreation.java
+++ java/src/main/java/scenarios/crewcreation/CharacterCreation.java
@@ -57,29 +57,6 @@ public final class CharacterCreation {
         );
     }
 
-    private static void printMeanScoreFlavorText (int meanScore) {
-        if (meanScore == 18) {
-            System.out.println("What?!?!?");
-            System.out.println("There's no way...");
-            System.out.println("I can't believe it! You've truly won the genetic lottery!");
-        } else if (meanScore >= 14) {
-            System.out.println("Very impressive! You remind me of myself back in my younger days...");
-        } else if (meanScore >= 10) {
-            System.out.println("Too good to be bad, but too bad to be good. You must have something to prove.");
-        } else if (meanScore == 9) {
-            System.out.println("I must say, you're exceptionally average.");
-            System.out.println("You're like a ruler for the rest of the universe to measure themselves against...");
-        } else if (meanScore >= 7) {
-            System.out.println("You're a bit below average, but I guess you've never let that stop you before...");
-        } else if (meanScore >= 4) {
-            System.out.println("These stats are quite low... life must be a bit of a struggle for you, huh?");
-        } else {
-            System.out.println("Wow... I don't think I've ever seen anything quite so terrible before.");
-            System.out.println("Just so you know, there's no shame in restarting the game.");
-            System.out.println("I probably would, if I were you...");
-        }
-    }
-
     private static Map<String, Integer> assignAbilityScores(List<Integer> scoresRolled) {
         Map<String, Integer> abilityScoreMap = new LinkedHashMap<>();
 
@@ -95,7 +72,7 @@ public final class CharacterCreation {
             List<Integer> availableScores = new ArrayList<>(scoresRolled);
             for (Map.Entry<String, Integer> ability : abilityScoreMap.entrySet()) {
                 String abilityName = ability.getKey();
-                System.out.println("");
+                System.out.println();
                 System.out.println("Which score would you like to assign to your [" + abilityName + "] ability?");
                 System.out.println("Enter the number corresponding to the value you wish to use...");
 
@@ -118,6 +95,53 @@ public final class CharacterCreation {
             }
         }
         return abilityScoreMap;
+    }
+
+    private static int selectScore(List<Integer> availableScores) {
+        // TODO: Depending on how Lanterna is set up, we should potentially extract the following code into a
+        //  utility method for players to select a choice from an ordered list
+        DataPrinter.printAsOrderedList(availableScores);
+
+        Integer selectedScore = null;
+        while (selectedScore == null) {
+            String invalidInputMessage = "Please enter a number from 1 to " + availableScores.size() + ".";
+
+            try {
+                selectedScore = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println(invalidInputMessage);
+                continue;
+            }
+            if (selectedScore < 1 || selectedScore > availableScores.size()) {
+                System.out.println(invalidInputMessage);
+                selectedScore = null;
+            }
+        }
+        // Subtracting 1 to match selection to desired index for the selected value
+        return selectedScore - 1;
+    }
+
+    private static void printMeanScoreFlavorText(int meanScore) {
+        if (meanScore == 18) {
+            System.out.println("What?!?!?");
+            System.out.println("There's no way...");
+            System.out.println("I can't believe it! You've truly won the genetic lottery!");
+        } else if (meanScore >= 14) {
+            System.out.println("Very impressive! You remind me of myself back in my younger days...");
+        } else if (meanScore >= 10) {
+            System.out.println("Too good to be bad, but too bad to be good. You must have something to prove.");
+        } else if (meanScore == 9) {
+            System.out.println("I must say, you're exceptionally average.");
+            System.out.println("You're like a ruler for the rest of the universe to measure themselves against...");
+        } else if (meanScore >= 7) {
+            System.out.println("You're a bit below average, but I guess you've never let that stop you before...");
+        } else if (meanScore >= 4) {
+            System.out.println("These stats are quite low... life must be a bit of a struggle for you, huh?");
+        } else {
+            System.out.println("Wow... I don't think I've ever seen anything quite so terrible before.");
+            System.out.println("Just so you know, there's no shame in restarting the game.");
+            System.out.println("I probably would, if I were you...");
+        }
     }
 
     private static void printAbilityScoreFlavorText(int abilityScoreValue, String abilityName) {
@@ -148,7 +172,7 @@ public final class CharacterCreation {
         System.out.println();
     }
 
-    private static void printPhysicalScoreFlavorText (int abilityScoreValue) {
+    private static void printPhysicalScoreFlavorText(int abilityScoreValue) {
         if (abilityScoreValue == 18) {
             // TODO: change bears to alien species once world is more fleshed out
             System.out.println("Holy smokes! You must wrestle with bears for fun!");
@@ -167,7 +191,7 @@ public final class CharacterCreation {
         }
     }
 
-    private static void printAgilityScoreFlavorText (int abilityScoreValue) {
+    private static void printAgilityScoreFlavorText(int abilityScoreValue) {
         if (abilityScoreValue == 18) {
             // TODO: change cobra to alien species once world is more fleshed out
             System.out.println("Whoa there! You must go bobbing for cobras instead of apples!");
@@ -184,7 +208,7 @@ public final class CharacterCreation {
         }
     }
 
-    private static void printBlasterScoreFlavorText (int abilityScoreValue) {
+    private static void printBlasterScoreFlavorText(int abilityScoreValue) {
         if (abilityScoreValue == 18) {
             System.out.println("Yee haw! You could shoot my eyebrow off with those moves!");
         } else if (abilityScoreValue >= 14) {
@@ -200,7 +224,7 @@ public final class CharacterCreation {
         }
     }
 
-    private static void printPilotingScoreFlavorText (int abilityScoreValue) {
+    private static void printPilotingScoreFlavorText(int abilityScoreValue) {
         if (abilityScoreValue == 18) {
             System.out.println("Wowee! You could fit a strike ship through a garbage chute!");
         } else if (abilityScoreValue >= 14) {
@@ -216,7 +240,7 @@ public final class CharacterCreation {
         }
     }
 
-    private static void printRepairScoreFlavorText (int abilityScoreValue) {
+    private static void printRepairScoreFlavorText(int abilityScoreValue) {
         if (abilityScoreValue == 18) {
             System.out.println("Golly! You could fix things before they even break!");
         } else if (abilityScoreValue >= 14) {
@@ -232,7 +256,7 @@ public final class CharacterCreation {
         }
     }
 
-    private static void printCharismaScoreFlavorText (int abilityScoreValue) {
+    private static void printCharismaScoreFlavorText(int abilityScoreValue) {
         if (abilityScoreValue == 18) {
             System.out.println("Wowzers! You could charm the pants off of me!");
         } else if (abilityScoreValue >= 14) {
@@ -248,30 +272,6 @@ public final class CharacterCreation {
                     "I probably wouldn't even notice you exist, if I weren't being payed to talk to you..."
             );
         }
-    }
-
-    private static int selectScore (List<Integer> availableScores) {
-        // TODO: Depending on how Lanterna is set up, we should potentially extract the following code into a
-        //  utility method for players to select a choice from an ordered list
-        DataPrinter.printAsOrderedList(availableScores);
-
-        Integer selectedScore = null;
-        while (selectedScore == null) {
-            String invalidInputMessage = "Please enter a number from 1 to " + availableScores.size() + ".";
-
-            try {
-                selectedScore = Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println(invalidInputMessage);
-                continue;
-            }
-            if (selectedScore < 1 || selectedScore > availableScores.size()) {
-                System.out.println(invalidInputMessage);
-                selectedScore = null;
-            }
-        }
-        // Subtracting 1 to match selection to desired index for the selected value
-        return selectedScore - 1;
     }
 
     private CharacterCreation() {}
